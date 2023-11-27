460337d497d184c8fae1dda631ad0a9ce35317ac
Amar Fadil <34912365+marfgold1@users.noreply.github.com>
2023-11-27 23:58:04 +0700

    maybe video

--- src/gui/controller/main.py
+++ src/gui/controller/main.py
@@ -3,15 +3,84 @@ import sys
 
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QFileDialog, QRadioButton, QButtonGroup,
-    QGraphicsScene, QErrorMessage, QMessageBox
+    QGraphicsScene, QMessageBox
 )
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QObject, pyqtSignal, QThread
 from PyQt5.QtGui import QPixmap, QImage
 import numpy as np
 from non_cnn.model import get_image
 from gui.views.mainWindow import Ui_MainGUI
 from gui.models import MODELS
 from PIL import Image
+import cv2
+
+
+class VideoWorker(QThread):
+    changeFrame = pyqtSignal(int)  # curFrame
+    changePlay = pyqtSignal(bool)  # isPlaying
+    togglePlay = pyqtSignal()
+    initialized = pyqtSignal(int)  # totalFrame
+    progress = pyqtSignal(int, Image.Image)  # curFrame, prediction
+    playToggled = pyqtSignal(bool)  # isPlaying
+
+    changeModel = pyqtSignal(dict)  # model
+
+    def __init__(self, media, model):
+        super().__init__()
+        self.model = model
+        self.media = cv2.VideoCapture(media)
+        self.isPlaying = False
+        self.curFrame = 0
+        self.totalFrame = int(self.media.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.initialized.emit(self.totalFrame)
+        self.changeFrame.connect(self._seek)
+        self.changePlay.connect(self._changePlay)
+        self.togglePlay.connect(self._togglePlay)
+        self.changeModel.connect(self._changeModel)
+        self.playToggled.emit(self.isPlaying)
+        # process first frame
+        self._process()
+
+    def _changeModel(self, model):
+        self.model = model
+
+    def _togglePlay(self):
+        print('toggle play')
+        self.isPlaying = not self.isPlaying
+        self.playToggled.emit(self.isPlaying)
+
+    def _changePlay(self, isPlaying):
+        print('change play', isPlaying)
+        self.isPlaying = isPlaying
+        self.playToggled.emit(self.isPlaying)
+
+    def _seek(self, frame_num):
+        if frame_num > -1:
+            self.media.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+            self.curFrame = frame_num
+        # process current frame
+        self._process()
+
+    def _process(self):
+        if self.media.isOpened():
+            ret, frame = self.media.read()
+            if not ret:
+                return False
+            m = self.model
+            predictions = m['predict'](m['model'], frame)
+            self.progress.emit(self.curFrame, predictions)
+            self.curFrame += 1
+            return True
+        return False
+
+    def run(self):
+        while True:
+            if not self.isPlaying:
+                continue
+            cur_play = self._process()
+            if self.isPlaying != cur_play:
+                self.isPlaying = cur_play
+                self.playToggled.emit(self.isPlaying)
 
 
 class Window(QMainWindow):
@@ -23,11 +92,14 @@ class Window(QMainWindow):
         self.setupModel()
         self.setup()
 
+        self.media = None
+        self.is_video = False
+        self.media_thread: QThread = None
+        self.media_worker: VideoWorker
+
     def setup(self):
         self.ui.mediaCntWidget.hide()
         self.ui.predictWidget.hide()
-        self.media = None
-        self.is_video = False
 
     def setupModel(self):
         self.current_model = list(MODELS.keys())[0]
@@ -68,7 +140,8 @@ class Window(QMainWindow):
         if not self.media:
             return
         if self.is_video:
-            self.setMediaControl(0, isPlaying=False, media=None)  # TODO
+            self.media_worker.changePlay.emit(False)
+            self.media_worker.changeFrame.emit(0)
         else:
             self.setImage(self.media)
             self.ui.predictLabel.setText('Click Predict to get prediction')
@@ -77,6 +150,10 @@ class Window(QMainWindow):
         if self.is_video and self.notSupportVideo(model_name):
             self.models[self.current_model]['button'].setChecked(True)
             return
+        if self.is_video and self.media:
+            self.media_worker.changeModel.emit(
+                self.models[model_name]
+            )
         self.current_model = model_name
         print('change model', model_name)
         self.resetPrediction()
@@ -93,7 +170,7 @@ class Window(QMainWindow):
         if not self.models[mdl]['video']:
             QMessageBox.critical(
                 self,
-                'Invalid Model', 
+                'Invalid Model',
                 f'Selected model {mdl} does not support video',
                 QMessageBox.Ok,
             )
@@ -110,6 +187,7 @@ class Window(QMainWindow):
                 self.ui.mediaCntWidget.show()
                 self.ui.predictWidget.hide()
                 self.is_video = True
+                self.initVideo(file_path)
             elif file_path.endswith(('.png', '.jpg', '.jpeg')):
                 self.ui.mediaCntWidget.hide()
                 self.ui.predictWidget.show()
@@ -119,6 +197,36 @@ class Window(QMainWindow):
             self.media = file_path
             self.resetPrediction()
             self.ui.filePathTxt.setText(file_path)
+
+    def initVideo(self, file_path):
+        if self.media_thread:
+            self.media_thread.quit()
+        self.media_thread = QThread()
+        self.media_worker = VideoWorker(
+            file_path,
+            self.models[self.current_model],
+        )
+        self.media_worker.moveToThread(self.media_thread)
+        self.media_thread.started.connect(self.media_worker.run)
+        self.media_thread.finished.connect(self.media_worker.deleteLater)
+        self.media_worker.initialized.connect(
+            lambda total_frame: self.ui.seekSlider.setRange(0, total_frame)
+        )
+        self.media_worker.progress.connect(
+            lambda cur_frame, prediction: self.setMediaControl(
+                curFrame=cur_frame,
+                media=prediction,
+            )
+        )
+        self.media_worker.playToggled.connect(
+            lambda is_playing: self.setMediaControl(isPlaying=is_playing)
+        )
+        self.ui.seekSlider.sliderMoved.connect(
+            lambda frame_num: self.media_worker.changeFrame.emit(
+                frame_num
+            )
+        )
+        self.media_thread.start()
 
     def setImage(self, file):
         scene = QGraphicsScene(self.ui.graphicsView)
@@ -157,23 +265,25 @@ class Window(QMainWindow):
         self.ui.graphicsView.fitInView(scene.sceneRect(),
                                         Qt.AspectRatioMode.KeepAspectRatio)
 
-    def setMediaControl(self, curFrame, isPlaying=False, media=None):
-        # self.ui.seekSlider.setRange(0, totalFrame)
-        # self.ui.seekSlider.setValue(curFrame)
-        # self.ui.timeLbl.setFormat(f'{curFrame}/{totalFrame}')
-        if media is not None:
+    def setMediaControl(self, curFrame=None, isPlaying=None, media=None):
+        if media:
+            print("get media frame:", curFrame)
             self.setImage(media)
-        if isPlaying:
-            self.ui.playBtn.setText('Pause')
-        else:
-            self.ui.playBtn.setText('Play')
+        if curFrame:
+            total_frame = self.media_worker.totalFrame
+            self.ui.seekSlider.setValue(curFrame)
+            self.ui.timeLbl.setText(f'{curFrame}/{total_frame}')
+        if isPlaying is not None:
+            if isPlaying:
+                self.ui.playBtn.setText('Pause')
+            else:
+                self.ui.playBtn.setText('Play')
 
     def playPause(self):
-        pass
+        self.media_worker.togglePlay.emit()
 
     def stop(self):
         self.resetPrediction()
-        # actually stop media
 
     def predict(self):
         file_path = self.ui.filePathTxt.text()
@@ -196,7 +306,6 @@ class Window(QMainWindow):
             self.ui.predictLabel.setText(
                 'Done predicting. Check the image.'
             )
-
 
 
 if __name__ == "__main__":
